7a26205e9400af99264a17c0ae2cbb6d6fec7b4c
numa trezzini <numa@trezzini.net>
2012-03-20 10:37:47 +0100

    first up, labo1

---
+++ Labo1/RobinetCreate.m
@@ -0,0 +1,35 @@
+Robinet=newfis("Un Robinet");
+
+% First input variable: debit
+CarAccel=addvar(CarAccel,'input','Speed',[-1,1]);
+% Memebership functions
+CarAccel=addmf(CarAccel,'input',1,'Slow','trapmf',[-2 -1 -0.5 0]);
+CarAccel=addmf(CarAccel,'input',1,'OK','trimf',[-0.5 0 0.1]);
+CarAccel=addmf(CarAccel,'input',1,'Fast','trapmf',[0 0.15 1 2]);
+
+% Second  input variable: debitchange
+CarAccel=addvar(CarAccel,'input','SpeedChange',[-1,1]);
+% Memebership functions
+CarAccel=addmf(CarAccel,'input',2,'Slowing','trapmf',[-2 -1 -0.3 0]);
+CarAccel=addmf(CarAccel,'input',2,'Constant','trimf',[-0.3 0 0.3]);
+CarAccel=addmf(CarAccel,'input',2,'Rising','trapmf',[0 0.3 1 2]);
+
+% Output variable: temperature
+CarAccel=addvar(CarAccel,'output','Action',[-1,1]);
+% Memebership functions
+CarAccel=addmf(CarAccel,'output',1,'Release','trimf',[-2 -1 0]);
+CarAccel=addmf(CarAccel,'output',1,'Nothing','trimf',[-1 0 1]);
+CarAccel=addmf(CarAccel,'output',1,'Push','trimf',[0 1 2]);
+
+% Rule creation
+MyRules = [...
+1 1 3 1 1;	% Push
+1 2 3 1 1;
+1 3 2 1 1; 	% Nothing
+2 1 2 1 1;
+2 2 2 1 1;
+2 3 2 1 1;
+3 1 2 1 1;
+3 2 1 1 1;	% Release
+3 3 1 1 1];
+CarAccel=addrule(CarAccel,MyRules);
